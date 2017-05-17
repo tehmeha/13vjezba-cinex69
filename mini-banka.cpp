9e4f8e366b4f3b48f57c09614eedf6bace62d809
Rimac1 <antoniorimac650@gmail.com>
2017-05-17 11:55:52 +0200

    Korisnik unosi novu osobu

--- mini-banka.cpp
+++ mini-banka.cpp
@@ -11,4 +11,18 @@ int main()
     cout << "5. Ispisi sortirano" << endl;
     int izbor;
     cin >> izbor;
+
+    unsigned long long int brBacuna[50];
+    string prezimeIme[50];
+    float saldov[50];
+    int brojKlijenata = 0;
+    if (izbor == 1)
+    {
+        cout << "Unesite broj racuna:";
+        cin >> brRacuna[brojKlijenata];
+        cout << "Unesite prezime i ime:" << endl;
+        getline(cin, prezimeIme[brojKlijenata]);
+        saldo[brojKlijenata] = 0;
+        brojKlijenta++;
+    }
 }
