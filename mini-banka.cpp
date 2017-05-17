ff349522ee8cdfe920209e869fcfdbc367396b7c
cinex <mateogamilec@gmail.com>
2017-05-17 12:14:58 +0200

    Pretraga prema broju racuna

--- mini-banka.cpp
+++ mini-banka.cpp
@@ -34,4 +34,18 @@ int main()
             cout << brRacuna[i] << " "<< prezimeIme[i] << "saldo:" << saldo[i] << endl;
         }
     }
+    else if(izboir == 3)
+    {
+        cout << "Unesite broj racuna koji zelite pronaci:" ;
+        unsigned long long int brRacunaKojegTrazimo;
+        cin >> brRacunaKojegTrazimo;
+        for(int i = 0; i < brojKlijenata == brRacuna[i])
+        {
+            if(brRacunaKojegTrazimo == brRacuna[i])
+            {
+                cout << "Osoba je pronadjena." << endl;
+                break;
+            }
+        }
+    }
 }
